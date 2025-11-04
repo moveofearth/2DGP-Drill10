92c08f38754877cae6ed7987a053a98ee5d31c1b
chikyuugaugokidasita <ansvia@naver.com>
2025-11-04 16:53:52 +0900

    Bird 클래스의 update 완성

--- bird.py
+++ bird.py
@@ -4,6 +4,17 @@ import game_world
 import game_framework
 import random
 
+TIME_PER_ACTION = 0.5
+ACTION_PER_TIME = 1.0 / TIME_PER_ACTION
+FRAMES_PER_ACTION = 8
+
+PIXEL_PER_METER = (10.0 / 0.3)
+RUN_SPEED_KMPH = 20.0
+RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
+RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
+RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
+
+
 class Bird:
     image = None
 
@@ -16,5 +27,16 @@ class Bird:
         self.frame = 0
         self.face_dir = 1
         self.dir = 0
-        self.speed = float(random.randint(50, 150))
 
+    def update(self):
+        self.x += self.dir * RUN_SPEED_PPS * game_framework.frame_time
+        if self.x < 0:
+            self.x = 0
+            self.dir = 1
+            self.face_dir = 1
+        elif self.x > 1600:
+            self.x = 1600
+            self.dir = -1
+            self.face_dir = -1
+
+        self.frame = (self.frame + 1) % 14
